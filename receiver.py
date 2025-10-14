26f3916589e7efff4ff8ddd4842c29654a600185
skarakuzu <karakuzu.seher@gmail.com>
2025-10-14 11:18:33 -0400

    changed so that encryption is optional

--- receiver.py
+++ receiver.py
@@ -2,29 +2,31 @@ import zmq
 import os
 import sys
 
-#server_pub = os.environ.get("SERVER_PUBLIC_KEY")
-#client_pub = os.environ.get("CLIENT_PUBLIC_KEY")
-#client_sec = os.environ.get("CLIENT_SECRET_KEY")
-
-server_pub = os.environ["SERVER_PUBLIC_KEY"].encode('ascii')
-client_pub = os.environ["CLIENT1_PUBLIC_KEY"].encode('ascii')
-client_sec = os.environ["CLIENT1_SECRET_KEY"].encode('ascii')
-
+try:
+    server_pub = os.environ["SERVER_PUBLIC_KEY"].encode('ascii')
+    client_pub = os.environ["CLIENT_PUBLIC_KEY"].encode('ascii')
+    client_sec = os.environ["CLIENT_SECRET_KEY"].encode('ascii')
+except:
+    pass
 
 context = zmq.Context()
 #socket = context.socket(zmq.SUB)
 socket = context.socket(zmq.PULL)
 
 #socket.setsockopt(zmq.CURVE_SERVER, True)
-socket.setsockopt(zmq.CURVE_PUBLICKEY, client_pub)
-socket.setsockopt(zmq.CURVE_SECRETKEY, client_sec)
-socket.setsockopt(zmq.CURVE_SERVERKEY, server_pub)
-
+try:
+    socket.setsockopt(zmq.CURVE_PUBLICKEY, client_pub)
+    socket.setsockopt(zmq.CURVE_SECRETKEY, client_sec)
+    socket.setsockopt(zmq.CURVE_SERVERKEY, server_pub)
+except: 
+    pass
 #socket.setsockopt(zmq.SUBSCRIBE, b"")
 adrr = sys.argv[1]
 socket.connect(adrr)
 print(f"listening the {adrr}")
+cnt = 1
 while True:
     msg = socket.recv_string()
-    print(f"Received the message: {msg}")
+    print(f"Received the {cnt}th message with content {msg}")
+    cnt += 1
 
